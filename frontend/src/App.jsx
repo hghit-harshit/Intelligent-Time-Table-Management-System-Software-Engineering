e639875bf6c34101691f786f7bd0554cb8185030
Rishikesh Kasireddy <rishikeshkasireddy@gmail.com>
2026-03-11 18:13:09 +0530

    Add Google Classroom integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,6 +19,10 @@ import AdminPage from "./pages/admin/AdminPage"
 import ExamSchedule from "./pages/ExamSchedule"
 import Notifications from "./pages/Notifications"
 import CourseEnrollment from "./pages/CourseEnrollment"
+import GoogleClassroom from "./pages/GoogleClassroom"
+// import AdminDashboard from "./pages/AdminDashboard"
+// import FacultyDashboard from "./pages/FacultyDashboard"
+// import StudentDashboard from "./pages/StudentDashboard"
 
 function App() {
   return (
@@ -35,6 +39,10 @@ function App() {
           <Route path="/exams" element={<ExamSchedule />} />
           <Route path="/notifications" element={<Notifications />} />
           <Route path="/courses" element={<CourseEnrollment />} />
+          <Route path="/google-classroom" element={<GoogleClassroom />} />
+          {/* <Route path="/admin" element={<AdminDashboard />} />
+          <Route path="/faculty" element={<FacultyDashboard />} />
+          <Route path="/student" element={<StudentDashboard />} /> */}
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
