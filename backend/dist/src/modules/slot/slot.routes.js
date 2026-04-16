d9edd23553a37f7b4b3fbb5f30f48f641467ac6a
muqeeth26832 <mdabdulmuqeeth26832@gmail.com>
2026-04-17 03:07:15 +0530

    Remove dist/ from git tracking

--- backend/dist/src/modules/slot/slot.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createSlot, deleteSlot, getAllSlots, getSlotById, updateSlot, } from "./slot.controller.js";
-const slotRouter = Router();
-slotRouter.get("/", getAllSlots);
-slotRouter.get("/:id", getSlotById);
-slotRouter.post("/", createSlot);
-slotRouter.put("/:id", updateSlot);
-slotRouter.delete("/:id", deleteSlot);
-export default slotRouter;
--- backend/dist/src/utils/hash.js
+++
@@ -1,4 +0,0 @@
-import { createHash } from "node:crypto";
-export const hash = (input) => {
-    return createHash("sha256").update(input).digest("hex");
-};
